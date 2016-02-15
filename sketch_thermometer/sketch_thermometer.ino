412d83e6c2d69efdf9fb85ab681f35e691815ae3
Danny No <dannyno@dannyno.33mail.com>
2016-02-15 18:45:26 +0100

    initial sketch file

---
+++ sketch_thermometer/sketch_thermometer.ino
@@ -0,0 +1,17 @@
+/*
+ * (c) 2016 Dannyno
+ * 
+ * Implements a thermometer funtion for arduino UNO
+ * 
+ * Version history:
+ * 2016-02-15 (dannyno): Created
+ */
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
